654471455b7bd8e7b7ece0ee560ccb608431a4fd
tanreet04 <146607959+tanreet04@users.noreply.github.com>
2025-07-19 16:15:18 +0530

    assignment Saturday

---
+++ practiceSaturday/practiceSaturdayTests/practiceSaturdayTests.swift
@@ -0,0 +1,17 @@
+//
+//  practiceSaturdayTests.swift
+//  practiceSaturdayTests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import practiceSaturday
+
+struct practiceSaturdayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
