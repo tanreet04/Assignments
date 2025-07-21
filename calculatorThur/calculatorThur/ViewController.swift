d9d31075b7604e6e526090a432fef30236c852f1
tanreet04 <146607959+tanreet04@users.noreply.github.com>
2025-07-21 13:53:42 +0530

    Calculator

---
+++ calculatorThur/calculatorThur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorThur
+//
+//  Created by Student on 17/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ calculatorThur/calculatorThurTests/calculatorThurTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorThurTests.swift
+//  calculatorThurTests
+//
+//  Created by Student on 17/07/25.
+//
+
+import Testing
+@testable import calculatorThur
+
+struct calculatorThurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
